93c268a1d47c16aab73aa720643e609cd2c10531
Harman Singh <sardarpreet001@gmail.com>
2025-02-21 09:47:46 +0530

    modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config}*/
-module.exports = {
-    content: ["./dist/*.html"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
